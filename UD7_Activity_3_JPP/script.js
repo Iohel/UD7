3579c37dcf0310abbe12b8db54d88bb0f745278d
Iohel <joelpons9@hotmail.com>
2024-02-26 19:27:58 +0100

    Comment code.

--- UD7_Activity_3_JPP/script.js
+++ UD7_Activity_3_JPP/script.js
@@ -3,6 +3,7 @@ import Headerpost from "./components/Header.js";
 import Footerpost from "./components/Footer.js";
 const { createApp } = Vue;
 
+
 createApp({
   data() {
     return {
@@ -24,9 +25,11 @@ createApp({
     Headerpost,
     Footerpost
   },
+  mounted() {
+    this.posts = JSON.parse(localStorage.getItem("posts"));
+  },
   methods: {
     createPost: function () {
-      /* let imageSaved = uploadAndSaveImage(); */
 
       if (this.post.title !== "" && this.post.author !== "") {
         this.post.summary = this.post.content.split(" ", 30).join(" ");
@@ -34,27 +37,22 @@ createApp({
         this.post.title = "";
         this.post.author = "";
         this.post.content = "";
-        
+        localStorage.setItem("posts",JSON.stringify(this.posts));
       }
+      
     },
     editPost: function (id) {
       id.title = this.post.title;
       id.author = this.post.author;
       id.content = this.post.content;
       id.summary = this.post.content.split(" ", 30).join(" ");
-      /* this.posts[id].image = uploadAndSaveImage(); */
+      localStorage.setItem("posts",JSON.stringify(this.posts));
     },
     deletePost: function (id) {
       console.log(this.posts.indexOf(id));
       this.posts.splice(this.posts.indexOf(id), 1);
+      localStorage.setItem("posts",JSON.stringify(this.posts));
     },
   },
 }).mount("#app");
 
-/* function uploadAndSaveImage() {
-  let imageUpload = document.getElementById("image");
-  let imageSaved = "";
-  if (imageUpload.files[0] !== undefined) {
-    return (imageSaved = URL.createObjectURL(imageUpload.files[0]));
-  }
-} */
